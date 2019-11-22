ccfca7090de7f0f23b487d0dd36b8e52893dd07e
kazuya0105 <55573361+kazuya0105@users.noreply.github.com>
2019-11-22 17:38:54 +0900

    Collision
    衝突判定を実装しました。

---
+++ 衝突判定/Assets/球.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 球 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody=GetComponent<Rigidbody>();
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            rigidbody.AddForce(-1f,0,0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            rigidbody.AddForce(1f,0,0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            rigidbody.AddForce(0,0,1f);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+             rigidbody.AddForce(0,0,-1f);
+        }
+    }
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag=="カプセル"){
+            Destroy(collision.gameObject);
+        }
+    }
+}
